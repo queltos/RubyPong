86528c6fa3ad6ead3bbc2f235753be368ad16f24
queltos <queltos@gmail.com>
2009-08-16 11:30:23 +0200

    deleted swap :)

--- lib/game_object.rb
+++ lib/game_object.rb
@@ -2,7 +2,7 @@ class GameObject
   attr_accessor :rect
   include Rubygame::Sprites::Sprite
   def initialize filename
-#    super
+    super()
     @image = Surface.load(filename)
     @rect = Rect.new(0,0,@image.size)
   end
