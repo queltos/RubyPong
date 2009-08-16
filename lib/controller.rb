6ef3724aff18f3d3d6ade064265df30d038f9d53
queltos <queltos@gmail.com>
2009-08-16 16:21:03 +0200

    this is an exercise ok..?

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,5 +1,25 @@
 class Controller
   include Rubygame::EventHandler::HasEventHandler
+  include Rubygame::EventTriggers
+  include Rubygame::EventActions
+
+  # Signals sprites to draw themselves on the screen
+  class DrawSprites
+    attr_accessor :screen
+    def initialize( screen )
+      @screen = screen
+    end
+  end
+
+  # Signals sprites to erase themselves from the screen
+  class UndrawSprites
+    attr_accessor :screen, :background
+    def initialize( screen, background )
+      @screen, @background = screen, background
+    end
+  end
+
+
   def initialize screen
     @screen = screen
     @queue = Rubygame::EventQueue.new
@@ -8,11 +28,34 @@ class Controller
     @clock.target_framerate = 30
     @background = Surface.load("data/playfield.png")
 
+    # initialize objects
     @ball = Ball.new
-    os = Sprites::Group.new
-    os.extend(Sprites::UpdateGroup)
-    os.push(@ball)
+    @objects = Sprites::Group.new
+    @objects.extend(Sprites::UpdateGroup)
+    @objects.push(@ball)
+    
+    class << @objects
+      include EventHandler::HasEventHandler
+
+      def do_draw(event)
+        dirty_rects = draw(event.screen)
+	event.screen.update_rects(dirty_rects)
+      end
+      
+      def do_undraw(event)
+        undraw(event.screen, event.background)
+      end
+    end
+
+    @objects.make_magic_hooks(	:tick	      => :update,
+    				DrawSprites   => :do_draw,
+				UndrawSprites => :do_undraw )
   end
+  
+  def update
+    puts "blub";
+  end
+
   def hook_quit
     quit_hooks = {
       :escape => :quit,
@@ -26,6 +69,7 @@ class Controller
   def run
     hook_quit
     loop do
+      @queue << UndrawSprites.new(@screen, @background)
       @queue.each do |event|
         handle(event)
       end
